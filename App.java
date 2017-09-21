f399dd6cda623dacf09ede5b430f22a2f6414dee
sanjay24in <sanjay24in@ymail.com>
2017-09-21 10:38:12 -0400

    updated file

--- App.java
+++ App.java
@@ -1,16 +1,24 @@
 package org.test;
 
 /**
- * Hello world!
+ * Application
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	/*final String XML_FILE_NAME = "C:\\Users\\Abhishek\\Desktop\\JobSearch\\Aviva\\SampleXML.xml";
+    	final String Excel_FILE_NAME = "C:\\Users\\Abhishek\\Desktop\\JobSearch\\Aviva\\TestCase.xlsx";
+    	*/
+    	
+        System.out.println( "Excel Read & Write" );
         
         ExcelReader reader = new ExcelReader();
         reader.readData();
-    }
+        
+        System.out.println("Adding data in xml");
+        Xmlreader readWriteXml = new Xmlreader();
+        readWriteXml.appendXMLTg("Start-Date","2017-09-21","company");
+    }	
 }
--- Xmlreader.java
+++ Xmlreader.java
@@ -3,10 +3,54 @@
  */
 package org.test;
 
+import org.w3c.dom.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+
 /**
- * @author Anand
+ * @author Abhishek
  *
  */
 public class Xmlreader {
+	
+	private static final String FILE_NAME = "C:\\Users\\Abhishek\\Desktop\\JobSearch\\Aviva\\SampleXML.xml";
 
+	public boolean appendXMLTg(String tagNamStr,String tagVal,String nodeName){
+		try{
+
+			String filepath = FILE_NAME;
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(filepath);
+			//Node name below which to add
+			Node nodeNameNd = doc.getElementsByTagName(nodeName).item(0);
+
+			Element tagNamElem = doc.createElement(tagNamStr);
+			tagNamElem.appendChild(doc.createTextNode(tagVal));
+			nodeNameNd.appendChild(tagNamElem);
+			
+			
+			System.out.println("Updated xml is below: \n"+doc.toString());
+
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+			return false;
+		}  catch (IOException ioe) {			
+			ioe.printStackTrace();
+			return false;
+		} catch (SAXException sae) {			
+			sae.printStackTrace();
+			return false;
+		} catch (Exception exe) {
+			exe.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
 }
